82a7639c93b3bd659b244e692b72da47ae418154
nerbertb <nerbertb@gmail.com>
2025-11-09 21:38:45 +0800

    feat: completed tablet UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 function Card(props) {
   return (
-    <div className="max-h-[250px] flex flex-col gap-8 p-8 bg-white drop-shadow-lg rounded-lg" style={{ borderTop: '4px solid', borderTopColor: props.color }}>
-      <div className='flex flex-col gap-1.5'>
-        <h3 className='poppins-semibold text-xl/[135%] text-[#4D4F62]'>{props.title}</h3>
-        <p className='text-[15px]/[140%] text-[#6A7178]'>{props.role}</p>
+    <div
+      className="max-h-[250px] max-w-[314px] flex flex-col gap-8 p-8 bg-white drop-shadow-lg rounded-lg"
+      style={{ borderTop: '4px solid', borderTopColor: props.color }}
+    >
+      <div className="flex flex-col gap-1.5 ">
+        <h3 className="poppins-semibold text-xl/[135%] text-[#4D4F62]">
+          {props.title}
+        </h3>
+        <p className="text-[15px]/[140%] text-[#6A7178]">{props.role}</p>
       </div>
-      <div className='flex justify-end'>
-        <img src={props.img} alt="" className='size-16'/>
+      <div className="flex justify-end">
+        <img src={props.img} alt="" className="size-16" />
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,13 +16,13 @@ function Home() {
 
   return (
     <main className='flex justify-center my-14'>
-      <div className="max-w-[375px] flex flex-col justify-center items-center gap-15">
-        <div className="max-w-[316px] flex flex-col gap-4">
-          <div>
-            <h1 className="text-[24px]/[140%] tracking-[0.25px] poppins-extralight text-[#4D4F62]">
+      <div className="max-w-[375px] flex flex-col justify-center items-center gap-15 md:max-w-3xl">
+        <div className="max-w-[316px] flex flex-col gap-4 md:max-w-[540px] md:text-center">
+          <div className='md:max-w-[540px]'>
+            <h1 className="text-[24px]/[140%] tracking-[0.25px] poppins-extralight text-[#4D4F62] md:text-4xl/[140%]">
               Reliable, efficient delivery
             </h1>
-            <h2 className="text-[24px]/[140%] tracking-[0.25px] poppins-semibold text-[#4D4F62]">
+            <h2 className="text-[24px]/[140%] md:text-4xl/[140%] tracking-[0.25px] poppins-semibold text-[#4D4F62]">
               Powered by Technology
             </h2>
           </div>
@@ -33,7 +33,12 @@ function Home() {
             </p>
           </div>
         </div>
-        <section className='max-w-[314px] flex flex-col gap-8'>{cards.map(createCards)}</section>
+        <section className='max-w-[314px] grid grid-cols-1 gap-8 md:max-w-[660px] md:grid-cols-2 md:auto-rows-auto md:place-items-center'>{cards.map((card, index) => {
+            const pos = index === 0 ? 'md:col-span-2 md:row-start-1' : index === 1 ? 'md:col-start-1 md:row-start-2' : index === 2 ? 'md:col-start-2 md:row-start-2' : 'md:col-span-2 md:row-start-3';
+            return (
+                <div key={card.title} className={pos}>{createCards(card)}</div>
+            )
+        })}</section>
       </div>
     </main>
   );
