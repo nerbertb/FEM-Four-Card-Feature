e704fc4b0ed4bff4c796738832f3d76af9bfc11c
nerbertb <nerbertb@gmail.com>
2025-11-09 20:36:07 +0800

    fix: border-top to 4px

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props) {
   return (
-    <div className="max-h-[250px] flex flex-col gap-8 p-8 bg-white drop-shadow-lg rounded-lg" style={{ borderTop: '5px solid', borderTopColor: props.color }}>
+    <div className="max-h-[250px] flex flex-col gap-8 p-8 bg-white drop-shadow-lg rounded-lg" style={{ borderTop: '4px solid', borderTopColor: props.color }}>
       <div className='flex flex-col gap-1.5'>
         <h3 className='poppins-semibold text-xl/[135%] text-[#4D4F62]'>{props.title}</h3>
         <p className='text-[15px]/[140%] text-[#6A7178]'>{props.role}</p>
