4c95f4eeccb02fef612e6a3d4f81d626c0834b3f
nerbertb <nerbertb@gmail.com>
2025-11-10 00:42:50 +0800

    fix: aria-label

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ function Card(props) {
       className="max-h-[250px] max-w-[314px] lg:max-w-[350px] flex flex-col gap-8 p-8 bg-white drop-shadow-lg rounded-lg"
       style={{ borderTop: '4px solid', borderTopColor: props.color }}
     >
-      <div className="flex flex-col gap-1.5 ">
+      <article aria-label={`${props.title} feature card`} className="flex flex-col gap-1.5 ">
         <h3 className="poppins-semibold text-xl/[135%] text-[#4D4F62]">
           {props.title}
         </h3>
         <p className="text-[15px]/[140%] text-[#6A7178]">{props.role}</p>
-      </div>
+      </article>
       <div className="flex justify-end">
         <img src={props.img} alt="" className="size-16" />
       </div>
